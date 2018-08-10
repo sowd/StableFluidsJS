785d1ad9dcb5802d309e565fcc5159f003317f16
Shigeru Owada <sgrowd@gmail.com>
2018-08-10 16:58:36 +0900

    Refactor + Perlin noise

--- js/main.js
+++ js/main.js
@@ -31,77 +31,45 @@ onload = function(){
     }
     animate();
 
-    // Define shape
-    setupGeometry(scene,128);
+    // Define volume
+    const xsiz = 128, ysiz = 128, zsiz = 128 ;
+    let srcVoxel = [];
+    for( let zi=0;zi<zsiz;++zi ){
+	srcVoxel.push([]);
+	for( let yi=0;yi<ysiz;++yi ){
+	    srcVoxel[zi].push([]);
+	    for( let xi=0;xi<xsiz;++xi ){
+		srcVoxel[zi][yi].push( [1,1,1,1] );
+		//let dist = Math.sqrt( _x*_x + _y*_y + _z*_z );
+		//let density = (dist < 1 ? 1.0-dist : 0) ; // ball
+		//srcVoxel[zi][yi].push( [density,density,density,density] );
+	    }
+	}
+    }
+
+    setPerlinNoise(srcVoxel,xsiz,ysiz,zsiz)
+
+
+    Render.allocate(xsiz,ysiz,zsiz);
+    Render.set4DFloatVoxelArray(srcVoxel);
+    Render.setScene(scene,128 /*zPlanes*/, 3.0 /*Brightness*/);
 
     // Render first frame
     renderer.render( scene, camera );
 };
 
-function setupGeometry(scene,numSlices){
-    const xsiz = 128, ysiz = 128, zsiz = 128 ;
-    const bUInt8 = true ;
-
-    const tx_width = xsiz * zsiz, tx_height = ysiz ;
-
-    // The volumetric texture is stored in single flat 2d texture
-    let vol = (bUInt8
-	       ? new Uint8Array( 4 * tx_width * tx_height )
-	       : new Float32Array( 4 * tx_width * tx_height ) );
-    function getVolIdx(ix,iy,iz){ return 4*( (iz*xsiz + ix)  + iy*(xsiz*zsiz) ) ;}
-
-    // Create test data
-    for( let zi=0;zi<zsiz;++zi ){
-	let _z = 2.0*zi/(zsiz-1) - 1 ; // -1 to 1
-	for( let yi=0;yi<ysiz;++yi ){
-	    let _y = 2.0*yi/(ysiz-1) - 1 ;
-	    for( let xi=0;xi<xsiz;++xi ){
-		let _x = 2.0*xi/(xsiz-1) - 1 ;
-		let dist = Math.sqrt( _x*_x + _y*_y + _z*_z );
-		let density = (dist < 1 ? 1.0-dist : 0) ; // ball
-		const iv = getVolIdx(xi,yi,zi);
-
-		if( bUInt8 ){
-		    vol[iv  ] = Math.floor(255*density+0.5);
-		    vol[iv+1] = Math.floor(255*density+0.5);
-		    vol[iv+2] = Math.floor(255*density+0.5);
-		    vol[iv+3] = Math.floor(255*density+0.5);
-		} else {
-		    vol[iv  ] = density ;
-		    vol[iv+1] = density ;
-		    vol[iv+2] = density ;
-		    vol[iv+3] = density ;
+function setPerlinNoise(srcVoxel,xsiz,ysiz,zsiz){
+    for( let rgba=0;rgba<4;++rgba ){
+	noise.seed(Math.random());
+	for( let zi=0;zi<zsiz;++zi ){
+	    let _z = 2.0*zi/(zsiz-1) - 1 ; // -1 to 1
+	    for( let yi=0;yi<ysiz;++yi ){
+		let _y = 2.0*yi/(ysiz-1) - 1 ;
+		for( let xi=0;xi<xsiz;++xi ){
+		    let _x = 2.0*xi/(xsiz-1) - 1 ;
+		    srcVoxel[zi][yi][xi][rgba] = noise.perlin3(_x,_y,_z) ;
 		}
 	    }
 	}
-    }
-
-    // Convert texture array to texture object
-    let texture = new THREE.DataTexture( vol, tx_width, tx_height, THREE.RGBAFormat );
-    texture.needsUpdate = true;
-
-
-    /////////////////////////////////////////////////////
-    // Geometry / material settings
-    // Vertex/fragment shaders defined in index.html & render.js
-    let material = genShaderMaterial(texture , zsiz /*texture z dim*/ , numSlices /*render plane number*/);
-
-    //  Set rendering geometry
-    const dz = 1.0 / numSlices ;
-    for( let z = -0.5 ; z <= 0.5 ; z += dz ){
-
-	const geometry = new THREE.Geometry();
-	const siz = 0.5 ;
-	geometry.vertices.push(new THREE.Vector3(-siz,  siz, z)); 
-	geometry.vertices.push(new THREE.Vector3( siz,  siz, z)); 
-	geometry.vertices.push(new THREE.Vector3( siz, -siz, z)); 
-	geometry.vertices.push(new THREE.Vector3(-siz, -siz, z)); 
-	geometry.faces.push(new THREE.Face3(0, 1, 2)); 
-	geometry.faces.push(new THREE.Face3(0, 3, 2));
-
-	//const geometry = new THREE.PlaneGeometry( 1, 1 ); mesh.position.z = z ;
-	//let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	let mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
     }
 }
--- js/render.js
+++ js/render.js
@@ -1,14 +1,91 @@
-function genShaderMaterial(texture , z_size , numSlices){
-    var ParamsShaderMaterial = {
-	uniforms: {
-	    "z_size": {value: z_size},
-	    "d_alpha": {value: 1.0/numSlices},
-	    "texture" : {type: "t" , value:texture }
-	},
-	vertexShader: $("#vertexshader")[0].textContent,
-	fragmentShader:  $("#fragmentshader")[0].textContent,
-	side: THREE.DoubleSide,
-	transparent: true
+const Render = {
+    allocate : function( xsiz , ysiz , zsiz , bUInt8){
+	this.bUInt8 = (bUInt8 !== false) ; // setting
+
+	this.xsiz = xsiz ; this.ysiz = ysiz ; this.zsiz = zsiz ;
+	this.tx_width = xsiz * zsiz , this.tx_height = ysiz ;
+
+	this.getVolIdx = function(ix,iy,iz){
+	    return 4*( (iz*xsiz + ix)  + iy*(xsiz*zsiz) ) ;
+	} ;
+	this.vol = (this.bUInt8
+		    ? new Uint8Array( 4 * this.tx_width * this.tx_height )
+		    : new Float32Array( 4 * this.tx_width * this.tx_height ) );
+    },
+    set4DFloatVoxelArray : function( vData /*[iz][iy][ix]*/ ){
+	const xsiz = vData[0][0].length ;
+	const ysiz = vData[0].length ;
+	const zsiz = vData.length ;
+
+	test.assert( xsiz == this.xsiz );
+	test.assert( ysiz == this.ysiz );
+	test.assert( zsiz == this.zsiz );
+	test.assert( this.vol != null );
+
+	// Copy txData to this.vol
+	for( let zi=0;zi<zsiz;++zi ){
+	    for( let yi=0;yi<ysiz;++yi ){
+		for( let xi=0;xi<xsiz;++xi ){
+		    const rgba = vData[zi][yi][xi];
+		    const vi = this.getVolIdx(xi,yi,zi);
+		    if( this.bUInt8 ){
+			this.vol[vi  ] = Math.floor(255*rgba[0]+0.5) ;
+			this.vol[vi+1] = Math.floor(255*rgba[1]+0.5) ;
+			this.vol[vi+2] = Math.floor(255*rgba[2]+0.5) ;
+			this.vol[vi+3] = Math.floor(255*rgba[3]+0.5) ;
+		    } else {
+			this.vol[vi  ] = rgba[0] ;
+			this.vol[vi+1] = rgba[1] ;
+			this.vol[vi+2] = rgba[2] ;
+			this.vol[vi+3] = rgba[3] ;
+		    }
+		}
+	    }
+	}
     }
-    return new THREE.ShaderMaterial(ParamsShaderMaterial);
-}
+    ,setScene : function(scene , numSlices  /*render plane number*/ , dAlphaMul){
+	test.assert( this.vol !== null ) ;
+	test.assert( this.xsiz !== null && this.ysiz !== null && this.zsiz !== null ) ;
+	test.assert( this.tx_width !== null && this.tx_height !== null ) ;
+	// Convert texture array to texture object
+	this.texture = new THREE.DataTexture( this.vol, this.tx_width, this.tx_height, THREE.RGBAFormat );
+	this.texture.needsUpdate = true;
+	this.numSlices = numSlices ;
+	if( dAlphaMul == null ) dAlphaMul = 1.0;
+
+	/////////////////////////////////////////////////////
+	// Geometry / material settings
+	// Vertex/fragment shaders defined in index.html
+	const ParamsShaderMaterial = {
+	    uniforms: {
+		"z_size": {value: this.zsiz},
+		"d_alpha": {value: dAlphaMul/this.numSlices},
+		"texture" : {type: "t" , value:this.texture }
+	    },
+	    vertexShader: $("#vertexshader")[0].textContent,
+	    fragmentShader:  $("#fragmentshader")[0].textContent,
+	    side: THREE.DoubleSide,
+	    transparent: true
+	}
+	this.material = new THREE.ShaderMaterial(ParamsShaderMaterial);
+
+	//  Set rendering geometry
+	const dz = 1.0 / numSlices ;
+	for( let z = -0.5 ; z <= 0.5 ; z += dz ){
+
+	    const geometry = new THREE.Geometry();
+	    const siz = 0.5 ;
+	    geometry.vertices.push(new THREE.Vector3(-siz,  siz, z)); 
+	    geometry.vertices.push(new THREE.Vector3( siz,  siz, z)); 
+	    geometry.vertices.push(new THREE.Vector3( siz, -siz, z)); 
+	    geometry.vertices.push(new THREE.Vector3(-siz, -siz, z)); 
+	    geometry.faces.push(new THREE.Face3(0, 1, 2)); 
+	    geometry.faces.push(new THREE.Face3(0, 3, 2));
+
+	    //const geometry = new THREE.PlaneGeometry( 1, 1 ); mesh.position.z = z ;
+	    //let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	    let mesh = new THREE.Mesh( geometry, this.material );
+	    scene.add( mesh );
+	}
+    }
+};
