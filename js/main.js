a9b2e5fc0e9009b967fd0fe6bc7e8e9f79802f8e
Shigeru Owada <sgrowd@gmail.com>
2018-08-10 11:17:18 +0900

    Virtual trackball from Three.js official examples

--- js/main.js
+++ js/main.js
@@ -1,84 +1,50 @@
 // Three.js docs: https://threejs.org/docs/index.html
 
 onload = function(){
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(
+    // Start creation of the whole scene
+    const scene = new THREE.Scene();
+
+
+    // Camera setup
+    const camera = new THREE.PerspectiveCamera(
 	50 /*FOV*/
 	, window.innerWidth / window.innerHeight /*Aspect ratio*/
 	, 0.1 /* Near clipping */, 1000 /*Far clipping*/ );
+    camera.position.z = 2;
 
-    var renderer = new THREE.WebGLRenderer();
+    // Basic renderer setup
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    // Viewpoint (camera) controll by virtual trackball.
+    // Usage described in:
+    // https://www.youtube.com/watch?v=4_KkHLimetQ
+    // http://irukanobox.blogspot.com/2016/06/threejstrackballcontrols.html
+    const ctrl = new THREE.TrackballControls( camera );
+    ctrl.addEventListener('change',()=>{renderer.render( scene, camera);});
 
-    var material = genShaderMaterial();
-    //var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 2;
-
-    virtualTrackBall(renderer,cube);
-
-
+    // Rendering loop
     function animate() {
 	requestAnimationFrame( animate );
-
-//	cube.rotation.x += 0.01;
-//	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
+	ctrl.update();
     }
     animate();
 
+    /////////////////////////////////////////////////////
+    // Geometry / material settings
+    // Vertex/fragment shaders defined in index.html & render.js
+    let material = genShaderMaterial();
+
+    //  Set rendering geometry
+    let meshes = [] ;
+    for( let z = -0.5 ; z <= 0.5 ; z += 0.1 ){
+	const geometry = new THREE.PlaneGeometry( 1, 1 );
+	//let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	//let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	let mesh = new THREE.Mesh( geometry, material );
+	mesh.position.z = z ;
+	scene.add( mesh );
+	meshes.push(mesh);
+    }
 };
-
-// Thanks to Eric @ https://jsfiddle.net/MadLittleMods/n6u6asza/
-function virtualTrackBall(renderer,cube){
-    var isDragging = false;
-    var previousMousePosition = {
-	x: 0,
-	y: 0
-    };
-    $(renderer.domElement).on('mousedown', function(e) {
-	isDragging = true;
-    }).on('mousemove', function(e) {
-	//console.log(e);
-	var deltaMove = {
-	    x: e.offsetX-previousMousePosition.x,
-	    y: e.offsetY-previousMousePosition.y
-	};
-
-	if(isDragging) {
-	    
-	    var deltaRotationQuaternion = new THREE.Quaternion()
-		.setFromEuler(new THREE.Euler(
-		    toRadians(deltaMove.y * 1),
-		    toRadians(deltaMove.x * 1),
-		    0,
-		    'XYZ'
-		));
-	    
-	    cube.quaternion.multiplyQuaternions(deltaRotationQuaternion, cube.quaternion);
-	}
-	
-	previousMousePosition = {
-	    x: e.offsetX,
-	    y: e.offsetY
-	};
-    });
-
-    $(document).on('mouseup', function(e) {
-	isDragging = false;
-    });
-
-    function toRadians(angle) {
-	return angle * (Math.PI / 180);
-    }
-
-    function toDegrees(angle) {
-	return angle * (180 / Math.PI);
-    }
-}
